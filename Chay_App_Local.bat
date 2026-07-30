c215a777160a24d4029619b01b5c69b94af91400
kieukieuvanvan1234-blip <kieukieuvanvan1234@gmail.com>
2026-07-30 11:38:44 +0700

    Add files via upload

---
+++ Chay_App_Local.bat
@@ -0,0 +1,11 @@
+@echo off
+title Khoi Chay App AIEdu_Video Local
+echo ==========================================================
+echo       DANG KHOI CHAY UNG DUNG AIEDU_VIDEO LOCAL
+echo ==========================================================
+echo.
+echo Vui long cho trong giay lat...
+echo.
+cd /d "%~dp0"
+npm run dev
+pause
